7f8b3f028058f1b494982f9f7c8f5c61506ab45d
SahilPatni17 <SahilPatni@Sahils-MacBook-Pro.local>
2017-02-16 21:13:21 +0530

    Initial

---
+++ HelloWorldApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorldApp
+//
+//  Created by Sahil Patni on 16/02/17.
+//  Copyright © 2017 Sahil Patni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorldApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorldApp
+//
+//  Created by Sahil Patni on 16/02/17.
+//  Copyright © 2017 Sahil Patni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
